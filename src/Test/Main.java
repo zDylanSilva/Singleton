270999eb5cab1d13f5c9c2e5aef062dbbacb080e
Dylan <dylandavid.silvaorrego@gmail.com>
2026-02-25 17:08:44 -0500

    Added Boiler to check Singleton principle

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -8,17 +8,20 @@ public class Main {
 		
 		Boiler boiler = Boiler.getInstance();
 		
+		//Another attempt of instance to check the logic flow still works since its the same object
+		Boiler anotherBoiler = Boiler.getInstance();
+		
 		//Procedure methods for output proof
 		System.out.println(boiler.addIngredients());
 		
 		System.out.println(boiler.startMixing());
 		
-		System.out.println(boiler.emptyContainer());
+		System.out.println(anotherBoiler.emptyContainer());
 		
 		//Failure test
 		
 		System.out.println("\nThe message below should send an error since it skips the ingredient method");
-		System.out.println(boiler.startMixing());
+		System.out.println(anotherBoiler.startMixing());
 		
 	}
 
